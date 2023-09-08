e0c88e0186c4bba70dea8c2079ccfbb377d444fa
xysong <xysong25@gmail.com>
2023-09-08 00:43:50 -0400

    Update: training vos

--- classification/CIFAR/train_virtual_dense.py
+++ classification/CIFAR/train_virtual_dense.py
@@ -32,30 +32,39 @@ parser.add_argument('--model', '-m', type=str, default='dense',
 parser.add_argument('--calibration', '-c', action='store_true',
                     help='Train a model to be used for calibration. This holds out some data for validation.')
 # Optimization options
-parser.add_argument('--epochs', '-e', type=int, default=100, help='Number of epochs to train.')
-parser.add_argument('--learning_rate', '-lr', type=float, default=0.1, help='The initial learning rate.')
-parser.add_argument('--batch_size', '-b', type=int, default=128, help='Batch size.')
+parser.add_argument('--epochs', '-e', type=int, default=100,
+                    help='Number of epochs to train.')
+parser.add_argument('--learning_rate', '-lr', type=float,
+                    default=0.1, help='The initial learning rate.')
+parser.add_argument('--batch_size', '-b', type=int,
+                    default=128, help='Batch size.')
 parser.add_argument('--test_bs', type=int, default=200)
 parser.add_argument('--momentum', type=float, default=0.9, help='Momentum.')
-parser.add_argument('--decay', '-d', type=float, default=0.0001, help='Weight decay (L2 penalty).')
+parser.add_argument('--decay', '-d', type=float,
+                    default=0.0001, help='Weight decay (L2 penalty).')
 # WRN Architecture
-parser.add_argument('--layers', default=40, type=int, help='total number of layers')
+parser.add_argument('--layers', default=40, type=int,
+                    help='total number of layers')
 parser.add_argument('--widen-factor', default=2, type=int, help='widen factor')
-parser.add_argument('--droprate', default=0.3, type=float, help='dropout probability')
+parser.add_argument('--droprate', default=0.3, type=float,
+                    help='dropout probability')
 # Checkpoints
-parser.add_argument('--save', '-s', type=str, default='./snapshots/baseline', help='Folder to save checkpoints.')
-parser.add_argument('--load', '-l', type=str, default='', help='Checkpoint path to resume / test.')
-parser.add_argument('--test', '-t', action='store_true', help='Test only flag.')
+parser.add_argument('--save', '-s', type=str,
+                    default='./snapshots/baseline', help='Folder to save checkpoints.')
+parser.add_argument('--load', '-l', type=str, default='',
+                    help='Checkpoint path to resume / test.')
+parser.add_argument('--test', '-t', action='store_true',
+                    help='Test only flag.')
 # Acceleration
 parser.add_argument('--ngpu', type=int, default=1, help='0 = CPU.')
-parser.add_argument('--prefetch', type=int, default=4, help='Pre-fetching threads.')
+parser.add_argument('--prefetch', type=int, default=1,
+                    help='Pre-fetching threads.')
 # energy reg
 parser.add_argument('--start_epoch', type=int, default=40)
 parser.add_argument('--sample_number', type=int, default=1000)
 parser.add_argument('--select', type=int, default=1)
 parser.add_argument('--sample_from', type=int, default=10000)
 parser.add_argument('--loss_weight', type=float, default=0.1)
-
 
 
 args = parser.parse_args()
@@ -75,12 +84,16 @@ train_transform = trn.Compose([trn.RandomHorizontalFlip(), trn.RandomCrop(32, pa
 test_transform = trn.Compose([trn.ToTensor(), trn.Normalize(mean, std)])
 
 if args.dataset == 'cifar10':
-    train_data = dset.CIFAR10('/nobackup-slow/dataset/my_xfdu/cifarpy', train=True, transform=train_transform, download=True)
-    test_data = dset.CIFAR10('/nobackup-slow/dataset/my_xfdu/cifarpy', train=False, transform=test_transform, download=True)
+    train_data = dset.CIFAR10('./Dataset/CIFAR-10',
+                              train=True, transform=train_transform, download=True)
+    test_data = dset.CIFAR10('./Dataset/CIFAR-10',
+                             train=False, transform=test_transform, download=True)
     num_classes = 10
 else:
-    train_data = dset.CIFAR100('/nobackup-slow/dataset/my_xfdu/cifarpy', train=True, transform=train_transform, download=True)
-    test_data = dset.CIFAR100('/nobackup-slow/dataset/my_xfdu/cifarpy', train=False, transform=test_transform, download=True)
+    train_data = dset.CIFAR100('/nobackup-slow/dataset/my_xfdu/cifarpy',
+                               train=True, transform=train_transform, download=True)
+    test_data = dset.CIFAR100('/nobackup-slow/dataset/my_xfdu/cifarpy',
+                              train=False, transform=test_transform, download=True)
     num_classes = 100
 
 
@@ -101,9 +114,10 @@ if args.model == 'allconv':
     net = AllConvNet(num_classes)
 elif args.model == 'dense':
     net = DenseNet3(100, num_classes, 12, reduction=0.5, bottleneck=True, dropRate=0.0, normalizer=None,
-                         k=None, info=None)
+                    k=None, info=None)
 else:
-    net = WideResNet(args.layers, num_classes, args.widen_factor, dropRate=args.droprate)
+    net = WideResNet(args.layers, num_classes,
+                     args.widen_factor, dropRate=args.droprate)
 
 
 start_epoch = 0
@@ -134,6 +148,8 @@ if args.dataset == 'cifar10':
     num_classes = 10
 else:
     num_classes = 100
+
+
 weight_energy = torch.nn.Linear(num_classes, 1).cuda()
 torch.nn.init.uniform_(weight_energy.weight)
 data_dict = torch.zeros(num_classes, args.sample_number, 342).cuda()
@@ -144,15 +160,14 @@ eye_matrix = torch.eye(342, device='cuda')
 logistic_regression = torch.nn.Linear(1, 2)
 logistic_regression = logistic_regression.cuda()
 optimizer = torch.optim.SGD(
-    list(net.parameters()) + list(weight_energy.parameters()) + \
+    list(net.parameters()) + list(weight_energy.parameters()) +
     list(logistic_regression.parameters()), state['learning_rate'], momentum=state['momentum'],
     weight_decay=state['decay'], nesterov=True)
 
 
 def cosine_annealing(step, total_steps, lr_max, lr_min):
     return lr_min + (lr_max - lr_min) * 0.5 * (
-            1 + np.cos(step / total_steps * np.pi))
-
+        1 + np.cos(step / total_steps * np.pi))
 
 
 def log_sum_exp(value, dim=None, keepdim=False):
@@ -179,6 +194,7 @@ def log_sum_exp(value, dim=None, keepdim=False):
 
 # /////////////// Training ///////////////
 
+
 def train(epoch):
     net.train()  # enter train mode
     loss_avg = 0.0
@@ -187,7 +203,6 @@ def train(epoch):
 
         # forward
         x, output = net.forward_virtual(data)
-
 
         # energy regularization.
         sum_temp = 0
@@ -200,24 +215,25 @@ def train(epoch):
             for index in range(len(target)):
                 dict_key = target_numpy[index]
                 data_dict[dict_key] = torch.cat((data_dict[dict_key][1:],
-                                                      output[index].detach().view(1, -1)), 0)
+                                                 output[index].detach().view(1, -1)), 0)
         elif sum_temp == num_classes * args.sample_number and epoch >= args.start_epoch:
             target_numpy = target.cpu().data.numpy()
             for index in range(len(target)):
                 dict_key = target_numpy[index]
                 data_dict[dict_key] = torch.cat((data_dict[dict_key][1:],
-                                                      output[index].detach().view(1, -1)), 0)
+                                                 output[index].detach().view(1, -1)), 0)
             # the covariance finder needs the data to be centered.
             for index in range(num_classes):
                 if index == 0:
                     X = data_dict[index] - data_dict[index].mean(0)
                     mean_embed_id = data_dict[index].mean(0).view(1, -1)
                 else:
-                    X = torch.cat((X, data_dict[index] - data_dict[index].mean(0)), 0)
+                    X = torch.cat(
+                        (X, data_dict[index] - data_dict[index].mean(0)), 0)
                     mean_embed_id = torch.cat((mean_embed_id,
                                                data_dict[index].mean(0).view(1, -1)), 0)
 
-            ## add the variance.
+            # add the variance.
             temp_precision = torch.mm(X.t(), X) / len(X)
             temp_precision += 0.0001 * eye_matrix
 
@@ -229,11 +245,13 @@ def train(epoch):
                 # breakpoint()
                 # index_prob = (prob_density < - self.threshold).nonzero().view(-1)
                 # keep the data in the low density area.
-                cur_samples, index_prob = torch.topk(- prob_density, args.select)
+                cur_samples, index_prob = torch.topk(
+                    - prob_density, args.select)
                 if index == 0:
                     ood_samples = negative_samples[index_prob]
                 else:
-                    ood_samples = torch.cat((ood_samples, negative_samples[index_prob]), 0)
+                    ood_samples = torch.cat(
+                        (ood_samples, negative_samples[index_prob]), 0)
             if len(ood_samples) != 0:
                 # add some gaussian noise
                 # ood_samples = self.noise(ood_samples)
@@ -243,7 +261,8 @@ def train(epoch):
                 # energy_score_for_bg = 1 * torch.logsumexp(predictions_ood[0][:, :-1] / 1, 1)
                 energy_score_for_bg = log_sum_exp(predictions_ood, 1)
 
-                input_for_lr = torch.cat((energy_score_for_fg, energy_score_for_bg), -1)
+                input_for_lr = torch.cat(
+                    (energy_score_for_fg, energy_score_for_bg), -1)
                 labels_for_lr = torch.cat((torch.ones(len(output)).cuda(),
                                            torch.zeros(len(ood_samples)).cuda()), -1)
 
@@ -258,7 +277,8 @@ def train(epoch):
             for index in range(len(target)):
                 dict_key = target_numpy[index]
                 if number_dict[dict_key] < args.sample_number:
-                    data_dict[dict_key][number_dict[dict_key]] = output[index].detach()
+                    data_dict[dict_key][number_dict[dict_key]
+                                        ] = output[index].detach()
                     number_dict[dict_key] += 1
 
         # backward
@@ -314,10 +334,10 @@ if not os.path.isdir(args.save):
     raise Exception('%s is not a dir' % args.save)
 
 with open(os.path.join(args.save, args.dataset + calib_indicator + '_' + args.model +
-'_' + str(args.loss_weight) + \
-                             '_' + str(args.sample_number)+ '_' + str(args.start_epoch) + '_' +\
-                            str(args.select) + '_' + str(args.sample_from) +
-                                  '_dense_baseline_training_results.csv'), 'w') as f:
+                       '_' + str(args.loss_weight) +
+                       '_' + str(args.sample_number) + '_' + str(args.start_epoch) + '_' +
+                       str(args.select) + '_' + str(args.sample_from) +
+                       '_dense_baseline_training_results.csv'), 'w') as f:
     f.write('epoch,time(s),train_loss,test_loss,test_error(%)\n')
 
 print('Beginning Training\n')
@@ -340,23 +360,24 @@ for epoch in range(start_epoch, args.epochs):
     # Save model
     torch.save(net.state_dict(),
                os.path.join(args.save, args.dataset + calib_indicator + '_' + args.model +
-                            '_baseline_dense'  + '_' + str(args.loss_weight) + \
-                             '_' + str(args.sample_number)+ '_' + str(args.start_epoch) + '_' +\
-                            str(args.select) + '_' + str(args.sample_from) + '_' + 'epoch_'  + str(epoch) + '.pt'))
+                            '_baseline_dense' + '_' + str(args.loss_weight) +
+                            '_' + str(args.sample_number) + '_' + str(args.start_epoch) + '_' +
+                            str(args.select) + '_' + str(args.sample_from) + '_' + 'epoch_' + str(epoch) + '.pt'))
     # Let us not waste space and delete the previous model
     prev_path = os.path.join(args.save, args.dataset + calib_indicator + '_' + args.model +
-                             '_baseline_dense' + '_' + str(args.loss_weight) + \
-                             '_' + str(args.sample_number)+ '_' + str(args.start_epoch) + '_' +\
-                            str(args.select) + '_' + str(args.sample_from)  + '_' + 'epoch_' + str(epoch - 1) + '.pt')
-    if os.path.exists(prev_path): os.remove(prev_path)
+                             '_baseline_dense' + '_' + str(args.loss_weight) +
+                             '_' + str(args.sample_number) + '_' + str(args.start_epoch) + '_' +
+                             str(args.select) + '_' + str(args.sample_from) + '_' + 'epoch_' + str(epoch - 1) + '.pt')
+    if os.path.exists(prev_path):
+        os.remove(prev_path)
 
     # Show results
 
     with open(os.path.join(args.save, args.dataset + calib_indicator + '_' + args.model +
-                                      '_' + str(args.loss_weight) + \
-                                      '_' + str(args.sample_number) + '_' + str(args.start_epoch) + '_' + \
-                                      str(args.select) + '_' + str(args.sample_from) +
-                                      '_dense_baseline_training_results.csv'), 'a') as f:
+                           '_' + str(args.loss_weight) +
+                           '_' + str(args.sample_number) + '_' + str(args.start_epoch) + '_' +
+                           str(args.select) + '_' + str(args.sample_from) +
+                           '_dense_baseline_training_results.csv'), 'a') as f:
         f.write('%03d,%05d,%0.6f,%0.5f,%0.2f\n' % (
             (epoch + 1),
             time.time() - begin_epoch,
